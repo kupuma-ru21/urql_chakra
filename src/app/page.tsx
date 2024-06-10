517177755514097ed30f0badbed7589100e18727
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-10 13:57:18 -0700

    feat: style props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-'use client'
-import { Link } from '@chakra-ui/next-js'
+"use client";
+import { Link } from "@chakra-ui/next-js";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function Page() {
   return (
-    <Link href='/about' color='blue.400' _hover={{ color: 'blue.500' }}>
-      To about
-    </Link>
-  )
+    <Box>
+      <Text
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        bgClip="text"
+        fontSize="6xl"
+        fontWeight="extrabold"
+      >
+        Welcome to Chakra UI
+      </Text>
+      <Link href="/about" color="blue.400" _hover={{ color: "blue.500" }}>
+        To about
+      </Link>
+    </Box>
+  );
 }
