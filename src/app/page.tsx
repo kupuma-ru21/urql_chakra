330c08c3f1d6d0ef02e87e733acfd95178cd06a9
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 16:30:58 -0700

    feat: add Custom Media queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,14 @@ import { Box, Heading, Image, Text } from "@chakra-ui/react";
 
 export default function Page() {
   return (
-    <Box borderWidth={2} borderColor="purple.500" p={5} className="my-box">
-      <Heading size="lg">
-        Hover the box...
-        <Box
-          as="span"
-          color="red.500"
-          sx={{
-            ".my-box:hover &": {
-              color: "green.500",
-            },
-          }}
-        >
-          And I will turn green!
-        </Box>
-      </Heading>
+    <Box
+      sx={{
+        "@media print": {
+          display: "none",
+        },
+      }}
+    >
+      This text won&apos;t be shown when printing this page.
     </Box>
   );
 }
