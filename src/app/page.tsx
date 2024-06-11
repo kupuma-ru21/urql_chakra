bb435ae361f306888785b4c021ab1a53e5333590
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 16:25:26 -0700

    feat: add Creating Nested Selectors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,20 @@ import { Box, Heading, Image, Text } from "@chakra-ui/react";
 
 export default function Page() {
   return (
-    <Box sx={{ "--my-color": "#000000" }}>
-      <Heading color="var(--my-color)" size="lg">
-        This uses CSS Custom Properties!
+    <Box borderWidth={2} borderColor="purple.500" p={5} className="my-box">
+      <Heading size="lg">
+        Hover the box...
+        <Box
+          as="span"
+          color="red.500"
+          sx={{
+            ".my-box:hover &": {
+              color: "green.500",
+            },
+          }}
+        >
+          And I will turn green!
+        </Box>
       </Heading>
     </Box>
   );
