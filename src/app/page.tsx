c44affc6fb07595e17103cac30c0b0760f0af999
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 16:15:53 -0700

    feat: add Custom Properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { Link } from "@chakra-ui/next-js";
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Heading, Image, Text } from "@chakra-ui/react";
 
 export default function Page() {
-  return <Box textStyle="h1">This is a box</Box>;
+  return (
+    <Box sx={{ "--my-color": "#000000" }}>
+      <Heading color="var(--my-color)" size="lg">
+        This uses CSS Custom Properties!
+      </Heading>
+    </Box>
+  );
 }
