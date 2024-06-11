dfcbe0725587649588dd0d0ac40b953bdc464d51
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 14:54:42 -0700

    feat: add Layer Styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,10 @@ import { Link } from "@chakra-ui/next-js";
 import { Box, Image, Text } from "@chakra-ui/react";
 
 export default function Page() {
-  return <a>global style</a>;
+  return (
+    <>
+      <Box layerStyle="selected">This is a box</Box>
+      <Box layerStyle="base">This is a box</Box>
+    </>
+  );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,18 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const theme = {
-  styles: {
-    global: {
-      "html, body": {
-        color: "gray.600",
-        lineHeight: "tall",
-      },
-      a: {
-        color: "teal.500",
-      },
+const theme = extendTheme({
+  layerStyles: {
+    base: {
+      bg: "gray.50",
+      border: "2px solid",
+      borderColor: "gray.500",
+    },
+    selected: {
+      bg: "teal.500",
+      color: "teal.700",
+      borderColor: "orange.500",
     },
   },
-};
+});
 
-export default extendTheme(theme);
+export default theme;
