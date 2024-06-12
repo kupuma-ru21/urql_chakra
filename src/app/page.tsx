600d6d551818cb8905b32a4a0dad674230ea618c
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 20:48:18 -0700

    feat: Responsive variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,13 @@
 import { Button } from "@chakra-ui/react";
 
 export default function Page() {
-  return <Button>Welcome</Button>;
+  return (
+    <>
+      <Button variant={{ base: "base", md: "md", sm: "sm" }}>
+        Object syntax
+      </Button>
+      {/* the button will be orange after the `sm` breakpoint due to the array syntax because The array syntax may not work as expected, since it ignores the naming of the variant and uses them straight in the order on which they are passed into the array. */}
+      <Button variant={["base", "md"]}>Array syntax</Button>
+    </>
+  );
 }
