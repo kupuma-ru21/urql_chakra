1d613b11242f62c79dde08c9cf4ca73a8a778f0f
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 20:12:11 -0700

    feat: Customizing component styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { Link } from "@chakra-ui/next-js";
-import { Box, Heading, Image, Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 export default function Page() {
-  return <Box bg="brand.100">Welcome</Box>;
+  return <Button>Welcome</Button>;
 }
