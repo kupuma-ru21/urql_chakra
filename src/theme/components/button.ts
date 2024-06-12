8f693196c3eaa4002e10bbdff3eb583eb7d86278
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-12 13:10:32 -0700

    feat: Scaling out your project

---
+++ src/theme/components/button.ts
@@ -0,0 +1,23 @@
+const button = {
+  sizes: {
+    sm: {
+      fontSize: "md",
+    },
+  },
+  variants: {
+    base: {
+      bg: "yellow.500",
+      fontSize: "md",
+    },
+    sm: {
+      bg: "teal.500",
+      fontSize: "lg",
+    },
+    md: {
+      bg: "orange.500",
+      fontSize: "xl",
+    },
+  },
+};
+
+export default button;
---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+import Button from "./components/button";
+
+const theme = extendTheme({
+  components: { Button },
+});
+
+export default theme;
