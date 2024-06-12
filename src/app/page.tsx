206e40b4ea4419f0293d0c89e089decd47a36225
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-11 17:38:46 -0700

    feat: Customizing theme tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,5 @@ import { Link } from "@chakra-ui/next-js";
 import { Box, Heading, Image, Text } from "@chakra-ui/react";
 
 export default function Page() {
-  return (
-    <Box
-      sx={{
-        "@media print": {
-          display: "none",
-        },
-      }}
-    >
-      This text won&apos;t be shown when printing this page.
-    </Box>
-  );
+  return <Box bg="brand.100">Welcome</Box>;
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  textStyles: {
-    h1: {
-      fontSize: ["48px", "72px"],
-      fontWeight: "bold",
-      lineHeight: "110%",
-      letterSpacing: "-2%",
-    },
-    h2: {
-      fontSize: ["36px", "48px"],
-      fontWeight: "semibold",
-      lineHeight: "110%",
-      letterSpacing: "-1%",
-    },
-  },
+  colors: { brand: { 100: "#f7fafc", 900: "#1a202c" } },
 });
 
 export default theme;
