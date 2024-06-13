64429d5a383f90a150a3e73df5775c95c8832c1b
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-13 13:41:00 -0700

    feat: Lazily mounting tab panels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,24 +10,21 @@ import {
 import { useState } from "react";
 
 export default function Page() {
-  const colors = useColorModeValue(
-    ["red.50", "teal.50", "blue.50"],
-    ["red.900", "teal.900", "blue.900"]
-  );
-  const [tabIndex, setTabIndex] = useState(0);
-  const bg = colors[tabIndex];
   return (
-    <Tabs onChange={(index) => setTabIndex(index)} bg={bg}>
+    <Tabs isLazy>
       <TabList>
-        <Tab>Red</Tab>
-        <Tab>Teal</Tab>
-        <Tab>Blue</Tab>
-        <Tab>{tabIndex}</Tab>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
       </TabList>
-      <TabPanels p="2rem">
-        <TabPanel>The Primary Colors</TabPanel>
-        <TabPanel>Are 1, 2, 3</TabPanel>
-        <TabPanel>Red, yellow and blue.</TabPanel>
+      <TabPanels>
+        {/* initially mounted */}
+        <TabPanel>
+          <p>one!</p>
+        </TabPanel>
+        {/* initially not mounted */}
+        <TabPanel>
+          <p>two!</p>
+        </TabPanel>
       </TabPanels>
     </Tabs>
   );
