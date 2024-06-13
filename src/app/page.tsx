5dcc943dbdd316f68c30e805fdeefe34d999f898
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-13 14:21:09 -0700

    feat: VisuallyHidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,12 @@
 "use client";
-import {
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { useState } from "react";
+import { CheckIcon } from "@chakra-ui/icons";
+import { Button, VisuallyHidden } from "@chakra-ui/react";
 
 export default function Page() {
   return (
-    <Tabs isLazy>
-      <TabList>
-        <Tab>One</Tab>
-        <Tab>Two</Tab>
-      </TabList>
-      <TabPanels>
-        {/* initially mounted */}
-        <TabPanel>
-          <p>one!</p>
-        </TabPanel>
-        {/* initially not mounted */}
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <Button>
+      <VisuallyHidden>Checkmark</VisuallyHidden>
+      <CheckIcon />
+    </Button>
   );
 }
