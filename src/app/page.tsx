a8b30013455b97e8a25681a6c444a07b72de178d
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-13 13:03:57 -0700

    feat: Tabs onChange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,34 @@
 "use client";
-import { Button, Heading, Image, Stack, Text } from "@chakra-ui/react";
-import { UserDefined } from "./components/UserDefined";
-import { Menu, MenuItem } from "./components/Menu";
+import {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Page() {
-  return (
-    <>
-      <Stack direction="row">
-        <Usage1 />
-        <Usage2 />
-      </Stack>
-      <Usage3 />
-      <Usage4 />
-    </>
+  const colors = useColorModeValue(
+    ["red.50", "teal.50", "blue.50"],
+    ["red.900", "teal.900", "blue.900"]
   );
-}
-
-function Usage1() {
+  const [tabIndex, setTabIndex] = useState(0);
+  const bg = colors[tabIndex];
   return (
-    <UserDefined>
-      <Image
-        src="https://chakra-ui.com/eric.jpg"
-        rounded="full"
-        w={32}
-        h={32}
-        boxShadow="md"
-        alt="usage1"
-      />
-      <Heading mt={6} maxW={60} size="lg" textAlign="center" color="gray.700">
-        Welcome back, Eric
-      </Heading>
-      <Text mt={6} mb={6} size="sm" color="blackAlpha.500">
-        Use your fingerprint to continue.
-      </Text>
-      <Image src="/207794.jpeg" w={32} h={32} alt="usage1-1" />
-    </UserDefined>
-  );
-}
-
-function Usage2() {
-  return (
-    <UserDefined variant="rounded">
-      <Image
-        src="https://chakra-ui.com/eric.jpg"
-        rounded="full"
-        w={32}
-        h={32}
-        boxShadow="md"
-        alt="usage2-1"
-      />
-      <Heading mt={6} maxW={60} size="lg" textAlign="center" color="gray.700">
-        Welcome back, Eric
-      </Heading>
-      <Text mt={6} mb={6} size="sm" color="blackAlpha.500">
-        Use your fingerprint to continue.
-      </Text>
-      <Image src="/207794.jpeg" w={32} h={32} alt="usage2-2" />
-    </UserDefined>
-  );
-}
-
-// 1. Using the default props defined in style config
-function Usage3() {
-  return (
-    <Menu size="sm" variant="bold">
-      <MenuItem>Awesome</MenuItem>
-      <MenuItem>Sauce</MenuItem>
-    </Menu>
-  );
-}
-
-// 2. Overriding the default
-function Usage4() {
-  return (
-    <Menu size="md" variant="colorful">
-      <MenuItem>Awesome</MenuItem>
-      <MenuItem>Sauce</MenuItem>
-    </Menu>
+    <Tabs onChange={(index) => setTabIndex(index)} bg={bg}>
+      <TabList>
+        <Tab>Red</Tab>
+        <Tab>Teal</Tab>
+        <Tab>Blue</Tab>
+        <Tab>{tabIndex}</Tab>
+      </TabList>
+      <TabPanels p="2rem">
+        <TabPanel>The Primary Colors</TabPanel>
+        <TabPanel>Are 1, 2, 3</TabPanel>
+        <TabPanel>Red, yellow and blue.</TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
