1532ad25d3923b44e0178803e642c82434930652
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-13 23:12:09 -0700

    chore: add Drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,50 @@
 "use client";
-import { CheckIcon } from "@chakra-ui/icons";
-import { Button, VisuallyHidden } from "@chakra-ui/react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Input,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRef } from "react";
 
 export default function Page() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef(null);
+
   return (
-    <Button>
-      <VisuallyHidden>Checkmark</VisuallyHidden>
-      <CheckIcon />
-    </Button>
+    <>
+      <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
