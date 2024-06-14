bf8b737b457ed1ac081150be3cab14a6ecd2357e
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-13 23:30:36 -0700

    chore: handle MenuItem as link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,16 @@
 "use client";
-import {
-  AddIcon,
-  EditIcon,
-  ExternalLinkIcon,
-  HamburgerIcon,
-  RepeatIcon,
-} from "@chakra-ui/icons";
-import {
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-} from "@chakra-ui/react";
-import { useRef } from "react";
+import { Link, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
 export default function Page() {
   return (
     <Menu>
-      <MenuButton
-        as={IconButton}
-        aria-label="Options"
-        icon={<HamburgerIcon />}
-        variant="outline"
-      />
+      <MenuButton>Open menu</MenuButton>
       <MenuList>
-        <MenuItem icon={<AddIcon />} command="⌘T">
-          New Tab
+        <MenuItem as={Link} href="/about">
+          To About
         </MenuItem>
-        <MenuItem icon={<ExternalLinkIcon />} command="⌘N">
-          New Window
-        </MenuItem>
-        <MenuItem icon={<RepeatIcon />} command="⌘⇧N">
-          Open Closed Tab
-        </MenuItem>
-        <MenuItem icon={<EditIcon />} command="⌘O">
-          Open File...
+        <MenuItem as={Link} href="/">
+          To Home
         </MenuItem>
       </MenuList>
     </Menu>
