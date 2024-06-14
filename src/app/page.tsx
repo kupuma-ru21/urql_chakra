aa7b38436ec34b04deeb01544b8035c3d1caa3ac
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-13 23:57:48 -0700

    chore: Control Focus when Modal closes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,48 @@
 "use client";
-import { Link, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRef } from "react";
 
 export default function Page() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = useRef(null);
+
   return (
-    <Menu>
-      <MenuButton>Open menu</MenuButton>
-      <MenuList>
-        <MenuItem as={Link} href="/about">
-          To About
-        </MenuItem>
-        <MenuItem as={Link} href="/">
-          To Home
-        </MenuItem>
-      </MenuList>
-    </Menu>
+    <>
+      <Box ref={finalRef} tabIndex={-1} aria-label="Focus moved to this box">
+        Some other content that&apos;ll receive focus on close.
+      </Box>
+
+      <Button mt={4} onClick={onOpen}>
+        Open Modal
+      </Button>
+      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest&apos;test
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
