a8286cd0f203c0187657a66ed44527953a7e65d1
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-15 22:42:23 -0700

    chore: Customizing rendered elements using useHighlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
 "use client";
 
-import { Text } from "@chakra-ui/react";
+import { Box, Heading, Mark, useHighlight } from "@chakra-ui/react";
 
 export default function Page() {
-  return (
-    <>
-      <Text noOfLines={1}>
-        &quot;The quick brown fox jumps over the lazy dog&quot; is an
-        English-language pangram—a sentence that contains all of the letters of
-        the English alphabet. Owing to its existence, Chakra was created.
-      </Text>
+  const chunks = useHighlight({
+    text: "With the Highlight component, you can spotlight, emphasize and accentuate words instantly",
+    query: ["spotlight", "emphasize", "accentuate", "instantly"],
+  });
 
-      <Text noOfLines={[1, 2, 3]}>
-        &quot;The quick brown fox jumps over the lazy dog&quot; is an
-        English-language pangram—a sentence that contains all of the letters of
-        the English alphabet. Owing to its existence, Chakra was created.
-      </Text>
-    </>
+  return (
+    <Heading lineHeight="tall">
+      {chunks.map(({ match, text }) => {
+        console.log("match", match);
+        console.log("text", text);
+        console.log("---------");
+
+        if (!match) return text;
+        return text === "instantly" ? (
+          <Box as="u" fontFamily="NewYork" key={text}>
+            {text}
+          </Box>
+        ) : (
+          <Mark
+            bg="black"
+            color="white"
+            fontFamily="NewYork"
+            px="2"
+            py="1"
+            key={text}
+          >
+            {text}
+          </Mark>
+        );
+      })}
+    </Heading>
   );
 }
