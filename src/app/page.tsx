160c2640f33c4a8b4c218b8c801c18448a913bba
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-16 13:33:26 -0700

    chore: useControllableState controllable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { Box, Button, useControllableState } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Page() {
-  const [value, setValue] = useControllableState({ defaultValue: 40 });
-  console.log("value", value);
+  // you need a state and updater to change the value
+  const [value, setValue] = useState(40);
+
+  const [internalValue, setInternalValue] = useControllableState({
+    value,
+    onChange: setValue,
+  });
+
+  console.log("internalValue", internalValue);
 
   return (
     <div>
-      <Button onClick={() => setValue(value + 1)}>+</Button>
+      <Button onClick={() => setInternalValue(value + 1)}>+</Button>
       <Box as="span" w="200px" mx="24px">
-        {value}
+        {internalValue}
       </Box>
-      <Button onClick={() => setValue(value - 1)}>-</Button>
+      <Button onClick={() => setInternalValue(value - 1)}>-</Button>
     </div>
   );
 }
