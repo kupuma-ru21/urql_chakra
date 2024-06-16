fd457786fb82622736a10b9b454429918f521aae
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-16 00:36:57 -0700

    chore: confirm useConst behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,14 @@
 "use client";
 
-import { Box, Heading, Mark, useHighlight } from "@chakra-ui/react";
+import { useConst } from "@chakra-ui/react";
 
 export default function Page() {
-  const chunks = useHighlight({
-    text: "With the Highlight component, you can spotlight, emphasize and accentuate words instantly",
-    query: ["spotlight", "emphasize", "accentuate", "instantly"],
-  });
-
+  const mountTime = useConst(() => new Date().toTimeString());
+  const obj = useConst({ a: 100 });
   return (
-    <Heading lineHeight="tall">
-      {chunks.map(({ match, text }) => {
-        console.log("match", match);
-        console.log("text", text);
-        console.log("---------");
-
-        if (!match) return text;
-        return text === "instantly" ? (
-          <Box as="u" fontFamily="NewYork" key={text}>
-            {text}
-          </Box>
-        ) : (
-          <Mark
-            bg="black"
-            color="white"
-            fontFamily="NewYork"
-            px="2"
-            py="1"
-            key={text}
-          >
-            {text}
-          </Mark>
-        );
-      })}
-    </Heading>
+    <>
+      <p>Mount time: {mountTime}</p>
+      <p>Value from constant object: {obj.a}</p>
+    </>
   );
 }
