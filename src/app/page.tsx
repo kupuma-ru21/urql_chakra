9e1874a221ede04afd175fa2487bedbdd32bf75a
kupuma-ru21 <kupumaru21@gmail.com>
2024-06-15 22:37:50 -0700

    chore: confirm Text behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { Button, Collapse, useDisclosure } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 export default function Page() {
-  const { isOpen, onToggle } = useDisclosure();
-
   return (
     <>
-      <Collapse startingHeight={20} in={isOpen}>
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-        terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-        labore wes anderson cred nesciunt sapiente ea proident.
-      </Collapse>
-      <Button size="sm" onClick={onToggle} mt="1rem">
-        Show {isOpen ? "Less" : "More"}
-      </Button>
+      <Text noOfLines={1}>
+        &quot;The quick brown fox jumps over the lazy dog&quot; is an
+        English-language pangram—a sentence that contains all of the letters of
+        the English alphabet. Owing to its existence, Chakra was created.
+      </Text>
+
+      <Text noOfLines={[1, 2, 3]}>
+        &quot;The quick brown fox jumps over the lazy dog&quot; is an
+        English-language pangram—a sentence that contains all of the letters of
+        the English alphabet. Owing to its existence, Chakra was created.
+      </Text>
     </>
   );
 }
